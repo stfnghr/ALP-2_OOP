e6c98aa644c916fa504c58d7d6d18b3cc7a901dc
Stefanie Agahari <161923734+stfnghr@users.noreply.github.com>
2024-05-27 12:45:14 +0700

    Update Admin.java

--- ALP_2/Admin.java
+++ ALP_2/Admin.java
@@ -5,13 +5,4 @@ public class Admin extends User {
     public Admin(int userID, String username, String email, String password) {
         super(userID, username, email, password);
     }
-
-    @Override
-    public void displayRole() {
-        System.out.println("Role: Admin");
-    }
-
-    public void createProject() {
-        // Implementasi untuk membuat proyek baru
-    }
 }
